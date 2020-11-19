81757aa9176e289cb63b5312f117eb3155c6dc22
tufailsher <tufail.sher@gmail.com>
2020-11-19 18:46:05 +0500

    tabs complete

---
+++ lib/widgets/call_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:law_chamber/main.dart';
+class CallLogs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: sizeConfig.height(0.5),
+      color: Colors.white,
+      child:Center(child: Text("Call Logs")),
+    );
+  }
+}
---
+++ lib/widgets/inquiries_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:law_chamber/main.dart';
+class InquiriesTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: sizeConfig.height(0.5),
+      color: Colors.white,
+      child:Center(child: Text("Inquiries")),
+    );
+  }
+}
